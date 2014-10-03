1a01b2f1523c1fffef5592ef46fd412ad5f5fc52
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2014-10-03 19:11:15 +0200

    Graph pro spravu grafu fixes #9

--- PPR/PPR/Game.cpp
+++ PPR/PPR/Game.cpp
@@ -11,47 +11,7 @@ Game::~Game()
 {
 }
 
-
-void Game::load(char* filepath)
+Graph& Game::getGraph()
 {
-	std::string line;
-	std::ifstream graf(filepath);
-	if (!graf.is_open())
-	{
-		std::cout << "[ERR] File cannot be opened!";
-		return;
-	}
-
-	getline(graf, line);
-	cnodes_ = std::stoi(line);
-	nodes_ = new Node[cnodes_];
-
-	for (int i = 0; i < cnodes_; ++i) {
-		std::cout << "[INF] Processing node " << i << " friends: ";
-		nodes_[i].init(i);
-		getline(graf, line);
-		for (std::string::size_type j = 0; j < line.size(); ++j) {
-			if (line[j] == '1') {
-				std::cout << j << ", ";
-				nodes_[i].addFriend(&nodes_[j]);
-			}
-		}
-		std::cout << std::endl;
-	}
-	graf.close();
+	return Graph_;
 }
-
-
-void Game::print()
-{
-	std::cout << "[INF] Printing nodes..." << std::endl;
-	std::cout << "Nodes count: " << cnodes_ << std::endl;
-	for (int i = 0; i < cnodes_; ++i) {
-		for (int j = 0; j < cnodes_; ++j) {
-			std::cout<< (nodes_[i].hasFriend(j) != 0) << " ";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "[INT] Nodes printed.";
-}
-
---
+++ PPR/PPR/Graph.cpp
@@ -0,0 +1,88 @@
+#include "stdafx.h"
+#include "Graph.h"
+
+
+Graph::Graph()
+{
+}
+
+
+Graph::~Graph()
+{
+}
+
+
+void Graph::load(char* filepath)
+{
+	std::string line;
+	std::ifstream graf(filepath);
+	if (!graf.is_open())
+	{
+		std::cout << "[ERR] File cannot be opened!";
+		return;
+	}
+
+	getline(graf, line);
+	cnodes_ = std::stoi(line);
+	nodes_ = new Node[cnodes_];
+	removed_nodes_ = new bool[cnodes_];
+
+	for (int i = 0; i < cnodes_; ++i) {
+		std::cout << "[INF] Processing node " << i << " friends: ";
+		nodes_[i].init(i);
+		getline(graf, line);
+		for (std::string::size_type j = 0; j < line.size(); ++j) {
+			if (line[j] == '1') {
+				std::cout << j << ", ";
+				nodes_[i].addFriend(&nodes_[j]);
+			}
+		}
+		removed_nodes_[i] = false;
+		std::cout << std::endl;
+	}
+	graf.close();
+}
+
+
+void Graph::print()
+{
+	std::cout << "[INF] Printing nodes..." << std::endl;
+	std::cout << "Nodes count: " << cnodes_ << std::endl;
+	for (int i = 0; i < cnodes_; ++i) {
+		if (removed_nodes_[i]) {
+			continue;
+		}
+
+		for (int j = 0; j < cnodes_; ++j) {
+			if (removed_nodes_[j]) {
+				continue;
+			}
+			std::cout << (nodes_[i].hasFriend(j) != 0) << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "[INF] Nodes printed." << std::endl;
+}
+
+
+void Graph::removeNode(Node* node)
+{
+	std::cout << "Removing node " << node->getNodeNumber() << "." << std::endl;
+	node->removeMyselfFromFriends();
+	removed_nodes_[node->getNodeNumber()] = true;
+}
+
+
+void Graph::restoreNode(Node* node)
+{
+	std::cout << "Restoring node " << node->getNodeNumber() << "." << std::endl;
+	node->addMyselfToFriends();
+	removed_nodes_[node->getNodeNumber()] = false;
+}
+
+
+Node* Graph::getNode(int node_number)
+{
+	return &nodes_[node_number];
+}
+
