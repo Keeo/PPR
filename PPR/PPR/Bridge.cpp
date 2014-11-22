a40cdc0c96d0b6db09d8cffa5a32cdfa9019de63
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2014-11-22 02:56:54 +0100

    ultrakokot

--- PPR/PPR/Bridge.cpp
+++ PPR/PPR/Bridge.cpp
@@ -22,12 +22,11 @@ void Bridge::setWork(char* data, int dataLength)
 	moves.assign(temp, temp + intCount);
 
 	LOG("bridge", "Size of dataLength: " + std::to_string(dataLength));
-#ifdef _DEBUG
 	std::stringstream ss;
 	for (auto &a : moves) {
 		ss << a << " ";
 	}
-#endif
+	Log::getInstance().info("bridge", "Prijata prace obsahuje: " + ss.str());
 	LOG("bridge", "Prijata prace obsahuje: " + ss.str());
 
 	game_.loadWork(moves);
